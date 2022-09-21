c36beb82fd129875f1f4f513455e7ed26efdab48
dev-ansh-r <rdevanshshukla26@gmail.com>
2022-09-21 08:45:18 +0530

    IFTTT email alert

---
+++ Day-5/esp-ifttt.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "wifi_name";
+const char* password = "wifi_password";
+const char* host = "maker.ifttt.com";
+
+void setup() {
+   Serial.begin(115200);
+   Serial.println("Email from Node Mcu");
+   delay(100);
+   Serial.print("Connecting to ");
+   Serial.println(ssid);
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+   }
+   Serial.println("");
+   Serial.println("IP address: ");
+   Serial.println(WiFi.localIP()); 
+}
+void loop() { 
+        WiFiClient client;
+        const int httpPort = 80; 
+         if (!client.connect(host, httpPort)) { 
+               Serial.println("connection failed"); 
+         return;}
+        if(Serial.available()){
+         char a=Serial.read();
+         if (a=='o'){
+                 String url = "/trigger/DoorBell/json/with/key/l46-WlcHj-ZRgY5ks-iYalb5PSFdPgk_Db1YlvIHZ5W";
+                 Serial.print("Requesting URL: ");
+                 Serial.println(url);
+                 client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                                   "Host: " + host + "\r\n" +  
+                                          "Connection: close\r\n\r\n");   
+                                   } 
+         else{
+           Serial.println("Correct character not pressed.Try again");
+         }
+         }
+         delay(1000);
+}
